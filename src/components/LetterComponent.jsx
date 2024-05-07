e7f7a910d64cc55a538359f7cd23cab72bf4feeb
VanyaMark <markova.vanya@gmail.com>
2024-05-07 15:41:22 +0200

    Created letterObj on LetterComponent with useState, next step - lifting state up to WordComponent in order to be able to compare with given word

--- src/components/LetterComponent.jsx
+++ src/components/LetterComponent.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const LetterComponent = ({index}) => {
-    const [letter, setLetter] = useState('')
+    const [letterObj, setLetterObj] = useState({index: index, letter: ''})
 
     const handleLetter = (event) => {
-        setLetter(event.target.value.slice(0,1))
-        console.log('handleLetter: ', letter)
+      const inputValue = event.target.value.slice(0,1).toUpperCase()
+        setLetterObj({...letterObj, letter: inputValue})
+        console.log('handleLetter: ', letterObj.letter)
     }
+
+    useEffect(() => {
+      console.log('letterObj: ', letterObj)
+    }, [letterObj])
+
   return (
-    <input className='letter' index={index} onChange={handleLetter} value={letter.toUpperCase()} maxLength={1} required>
+    <input className='letter' index={index} onChange={handleLetter} value={letterObj.letter} maxLength={1} required>
     </input>
   )
 }
 
 export default LetterComponent
 
-{/* <input type="number" id="initialInvestment" name="initialInvestment" value={investmentValue.initialInvestment} onChange={handleInvestmentChange} required></input> */}
-
-// const handleInvestmentChange = (event) => {
-//     setInvestmentValue(prevInvestmentValue => {
-//         return {...prevInvestmentValue,
-//                 [event.target.id]: Number(event.target.value)}
-//     })
-//     console.log(investmentValue)
-// }
