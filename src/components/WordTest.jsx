9fcb809cd0afd1a76931ddc61080b6cf0b0f2986
VanyaMark <markova.vanya@gmail.com>
2024-05-09 15:24:29 +0200

    wordTest component with word array state functioning correctly, next step lifting state up to app

--- src/components/WordTest.jsx
+++ src/components/WordTest.jsx
@@ -1,41 +1,29 @@
 import React from "react";
 import { useState } from 'react';
 
+const indexArr = [0, 1, 2, 3, 4];
+
 const WordTest = () => {
     const [word, setWord] = useState([])
 
-    const handleWord = (event) => {
-
+    const handleWord = (event, index) => {
+      const letter = event.target.value.toUpperCase();
+      const newWord = [...word];
+      newWord[index] = letter;
+      setWord(newWord);
+      console.log('word from wordTest comp: ', word)
     }
 
   return (
     <div class="center">
-      <input
-        onChange={handleWord}
+      {indexArr.map(index =>       <input
+        key={index} 
+        onChange={event => handleWord(event, index)}
         className="letter"
+        value={word[index]}
         maxLength={1}
         required
-      ></input>
-      <input
-        className="letter"
-        maxLength={1}
-        required
-      ></input>
-      <input
-        className="letter"
-        maxLength={1}
-        required
-      ></input>
-      <input
-        className="letter"
-        maxLength={1}
-        required
-      ></input>
-      <input
-        className="letter"
-        maxLength={1}
-        required
-      ></input>
+      ></input>)}
     </div>
   );
 };
